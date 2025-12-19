e57db4315732907880f6529687d1ecc70922f6a1
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-12-19 12:21:30 +0300

    refactor : edit the source code
    * Edited the task number one

--- src/main/java/org/java/mikhail/tasks/Task1.java
+++ src/main/java/org/java/mikhail/tasks/Task1.java
@@ -3,53 +3,74 @@ package org.java.mikhail.tasks;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+/**
+ * @author Mikhail.
+ * <p>Решение задачи №1.</p>
+ */
 public class Task1 {
 
+    /**
+     * Точка входа в программу
+     *
+     * @param args список аргументов командной строки.
+     */
     public static void main(String[] args) {
-        double[] array = {2,1,4,2,3};
+        double[] array = {2, 1, 4, 2, 3, 18, 10, 18};
         double[] result = Task1.task(array);
 
         System.out.println(Arrays.toString(result));
     }
 
-    // Последний раз
+    /**
+     * Удаляет дубликаты из исходного массива.
+     *
+     * @param a массив чисел, длина массива может быть больше 10 млн.
+     * @return массив чисел, в которых отсутствуют дубликаты.
+     * Порядок чисел в оригинальном массиве должен быть сохранён.
+     * Из дубликатов нужно оставлять последний элемент, например, для {@code {2,1,4,2,3}}
+     * <b>правильное решение - {@code {1,4,2,3}}</b>,
+     * а не {@code {2,1,4,3}}
+     * @throws IllegalArgumentException исключение, которое возникает, когда во входном массиве
+     *                                  есть элемент меньше нуля.
+     */
     public static double[] task(double[] a) {
         if (a.length == 0) return new double[0];
 
-        for(double val : a) {
-            if (val < 0)
+        for (double val : a) {
+            if (val < 0) {
                 throw new IllegalArgumentException("Массив содержит отрицательное значение: " + val);
+            }
         }
 
-        // put() перезапишет предыдущее, сохранится индекс посл.вх.
-        // 2. Запомним последнее вхождение каждого элемента
+        final List<Double> resultList = getDoubleList(a);
+
+        double[] result = new double[resultList.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+
+    private static List<Double> getDoubleList(double[] a) {
+        // Метод put() перезапишет предыдущее, сохранится индекс последнего вхождения.
+        // Запоминаем последнее вхождение каждого элемента
         Map<Double, Integer> lastIndexMap = new HashMap<>();
         for (int i = 0; i < a.length; i++) {
             lastIndexMap.put(a[i], i);
         }
 
         // O(n)
-        // 3. Добавим в результат только те элементы, чей индекс совпадает с последним вхождением
+        // Добавляем в результат только те элементы, чей индекс совпадает с последним вхождением
         List<Double> resultList = new ArrayList<>();
-        Set<Double> added = new HashSet<>();
         for (int i = 0; i < a.length; i++) {
             double val = a[i];
-            if (lastIndexMap.get(val) == i && !added.contains(val)) {
+            if (lastIndexMap.get(val) == i && !resultList.contains(val)) {
                 resultList.add(val);
-                added.add(val);
             }
         }
-
-        // 4. Преобразуем в примитивный массив
-        double[] result = new double[resultList.size()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = resultList.get(i);
-        }
-        return result;
+        return resultList;
     }
 }
